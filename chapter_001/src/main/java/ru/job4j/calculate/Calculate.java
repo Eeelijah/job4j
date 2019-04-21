ad26adb44ca71e6cfd3512b17c330b9a0514423d
Ilya Balov <ilays2011@gmail.com>
2019-04-21 22:00:28 +0300

    1.3. JavaDoc. Code convention[#136002]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j.calculate;
 
+/**
+  * Class Класс для вычислений арифметических операций + - * / ^.
+  * @author balov.ilya
+  * @since 21.04.2019
+  * @version 1
+*/
 public class Calculate {
-	
+	/**
+	  * Точка входа в программу.
+	  * @param args - строковый массив.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Ilya Balov
+* @version 1.1
+* @since 0.1
+*/
+package ru.job4j.calculate;
