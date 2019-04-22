eb2fed07ce751aa6e3fb392f5482256903a846d3
Ilya Balov <ilays2011@gmail.com>
2019-04-22 20:35:56 +0300

    1.4. JUnit[#136003]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,17 +1,26 @@
 package ru.job4j.calculate;
 
 /**
-  * Class Класс для вычислений арифметических операций + - * / ^.
-  * @author balov.ilya
-  * @since 21.04.2019
-  * @version 1
-*/
+ * Class Класс для вычислений арифметических операций + - * / ^.
+ * @author balov.ilya
+ * @since 21.04.2019
+ * @version 1
+ */
 public class Calculate {
 	/**
-	  * Точка входа в программу.
-	  * @param args - строковый массив.
+	 * Точка входа в программу.
+	 * @param args - строковый массив.
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+	}
+	
+	/**
+	 * Method echo.
+	 * @param name Your name.
+	 * @return Echo plus your name.
+	 */
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
 	}
 }
