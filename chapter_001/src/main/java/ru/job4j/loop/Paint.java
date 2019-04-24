2ab1a2c22d4ab4c6f016a4c698463da88330fee9
Ilya Balov <ilays2011@gmail.com>
2019-04-24 22:16:38 +0300

    5.4. Построить пирамиду в псевдографике.[#135973]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -0,0 +1,65 @@
+package ru.job4j.loop;
+
+/**
+ * Класс для построения пирамиды.
+ *
+ * @author Ilya Balov
+ * @version 1
+ * @since 24.04.2019
+ */
+public class Paint {
+
+    /**
+     * Метод для построения пирамиды.
+     *
+     * @param height высота пирамиды
+     * @return строка, представляющая пирамиду
+     */
+    public String rightTrl(int height) {
+        StringBuilder screen = new StringBuilder();
+        int width = height;
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != width; column++) {
+                if (row >= column) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+
+    public String leftTrl(int height) {
+        StringBuilder screen = new StringBuilder();
+        int width = height;
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != width; column++) {
+                if (row >= width - column - 1) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+
+    public String pyramid(int height) {
+        StringBuilder screen = new StringBuilder();
+        int width = 2 * height - 1;
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != width; column++) {
+                if (row >= height - column - 1 && row + height - 1 >= column) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -0,0 +1,78 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import java.util.StringJoiner;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Test for PaintTest class.
+ *
+ * @author Ilya Balov
+ * @version 24.04.2015
+ * @since 1
+ */
+public class PaintTest {
+
+    /**
+     * Test rightTrl method
+     */
+    @Test
+    public void whenPyramid4Right() {
+        Paint paint = new Paint();
+        String rst = paint.rightTrl(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("^   ")
+                                .add("^^  ")
+                                .add("^^^ ")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    /**
+     * Test leftTrl method
+     */
+    @Test
+    public void whenPyramid4Left() {
+        Paint paint = new Paint();
+        String rst = paint.leftTrl(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^")
+                                .add("  ^^")
+                                .add(" ^^^")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    /**
+     * Test pyramid method
+     */
+    @Test
+    public void whenPyramid4Full() {
+        Paint paint = new Paint();
+        String rst = paint.pyramid(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^   ")
+                                .add("  ^^^  ")
+                                .add(" ^^^^^ ")
+                                .add("^^^^^^^")
+                                .toString()
+                )
+        );
+    }
+}
