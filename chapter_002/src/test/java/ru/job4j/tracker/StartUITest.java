3925a87d40eb7f70f13bba1589f5c478c2276545
Ilya Balov <ilays2011@gmail.com>
2019-05-01 21:10:23 +0300

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.[#136039]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,12 +1,30 @@
 package ru.job4j.tracker;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class StartUITest {
+
+    private PrintStream stdout = System.out;
+    private ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+    }
 
     @Test
     public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
@@ -32,5 +50,127 @@ public class StartUITest {
         Input input = new StubInput(new String[]{"3", item.getId(), "6"});
         new StartUI(input, tracker).init();
         assertThat(tracker.findById(item.getId()), is(nullValue()));
+    }
+
+    @Test
+    public void whenShowAllItems() {
+        Tracker tracker = new Tracker();
+        Item item1 = tracker.add(new Item("test name1", "desc1", System.currentTimeMillis()));
+        Item item2 = tracker.add(new Item("test name2", "desc2", System.currentTimeMillis()));
+        Item item3 = tracker.add(new Item("test name3", "desc3", System.currentTimeMillis()));
+        Input input = new StubInput(new String[]{"1", "6"});
+        new StartUI(input, tracker).init();
+        String separator = System.lineSeparator();
+
+        assertThat(
+                this.out.toString(),
+                is(
+                        new StringBuilder()
+                                .append(separator)
+                                .append("МЕНЮ ВЫБОРА:").append(separator)
+                                .append("0. Добавить новую заявку").append(separator)
+                                .append("1. Показать все заявки").append(separator)
+                                .append("2. Редактировать заявку").append(separator)
+                                .append("3. Удалить заявку").append(separator)
+                                .append("4. Найти заявку по id").append(separator)
+                                .append("5. Найти заявку по имени").append(separator)
+                                .append("6. Выйти из программы").append(separator)
+                                .append("------------ Вывод всех заявок --------------").append(separator)
+                                .append(item1.toString()).append(separator)
+                                .append(item2.toString()).append(separator)
+                                .append(item3.toString()).append(separator)
+                                .append(separator)
+                                .append("МЕНЮ ВЫБОРА:").append(separator)
+                                .append("0. Добавить новую заявку").append(separator)
+                                .append("1. Показать все заявки").append(separator)
+                                .append("2. Редактировать заявку").append(separator)
+                                .append("3. Удалить заявку").append(separator)
+                                .append("4. Найти заявку по id").append(separator)
+                                .append("5. Найти заявку по имени").append(separator)
+                                .append("6. Выйти из программы").append(separator)
+                                .append("------------ Завершение работы программы... --------------").append(separator)
+                                .toString()
+                )
+        );
+    }
+
+    @Test
+    public void whenFindById() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name1", "desc1", System.currentTimeMillis()));
+        Input input = new StubInput(new String[]{"4", item.getId(), "6"});
+        new StartUI(input, tracker).init();
+        String separator = System.lineSeparator();
+
+        assertThat(
+                this.out.toString(),
+                is(
+                        new StringBuilder()
+                                .append(separator)
+                                .append("МЕНЮ ВЫБОРА:").append(separator)
+                                .append("0. Добавить новую заявку").append(separator)
+                                .append("1. Показать все заявки").append(separator)
+                                .append("2. Редактировать заявку").append(separator)
+                                .append("3. Удалить заявку").append(separator)
+                                .append("4. Найти заявку по id").append(separator)
+                                .append("5. Найти заявку по имени").append(separator)
+                                .append("6. Выйти из программы").append(separator)
+                                .append("------------ Поиск заявки по id --------------").append(separator)
+                                .append("------------ Найдена заявка: " + item.toString() + ". -----------").append(separator)
+                                .append(separator)
+                                .append("МЕНЮ ВЫБОРА:").append(separator)
+                                .append("0. Добавить новую заявку").append(separator)
+                                .append("1. Показать все заявки").append(separator)
+                                .append("2. Редактировать заявку").append(separator)
+                                .append("3. Удалить заявку").append(separator)
+                                .append("4. Найти заявку по id").append(separator)
+                                .append("5. Найти заявку по имени").append(separator)
+                                .append("6. Выйти из программы").append(separator)
+                                .append("------------ Завершение работы программы... --------------").append(separator)
+                                .toString()
+                )
+        );
+    }
+
+    @Test
+    public void whenFindByName() {
+        Tracker tracker = new Tracker();
+        Item item1 = tracker.add(new Item("test name1", "desc1", System.currentTimeMillis()));
+        Item item2 = tracker.add(new Item("test name1", "desc2", System.currentTimeMillis()));
+        Item item3 = tracker.add(new Item("test name3", "desc3", System.currentTimeMillis()));
+        Input input = new StubInput(new String[]{"5", item1.getName(), "6"});
+        new StartUI(input, tracker).init();
+        String separator = System.lineSeparator();
+
+        assertThat(
+                this.out.toString(),
+                is(
+                        new StringBuilder()
+                                .append(separator)
+                                .append("МЕНЮ ВЫБОРА:").append(separator)
+                                .append("0. Добавить новую заявку").append(separator)
+                                .append("1. Показать все заявки").append(separator)
+                                .append("2. Редактировать заявку").append(separator)
+                                .append("3. Удалить заявку").append(separator)
+                                .append("4. Найти заявку по id").append(separator)
+                                .append("5. Найти заявку по имени").append(separator)
+                                .append("6. Выйти из программы").append(separator)
+                                .append("------------ Поиск заявки по имени --------------").append(separator)
+                                .append("------------ Найдены заявки: -----------").append(separator)
+                                .append(item1.toString()).append(separator)
+                                .append(item2.toString()).append(separator)
+                                .append(separator)
+                                .append("МЕНЮ ВЫБОРА:").append(separator)
+                                .append("0. Добавить новую заявку").append(separator)
+                                .append("1. Показать все заявки").append(separator)
+                                .append("2. Редактировать заявку").append(separator)
+                                .append("3. Удалить заявку").append(separator)
+                                .append("4. Найти заявку по id").append(separator)
+                                .append("5. Найти заявку по имени").append(separator)
+                                .append("6. Выйти из программы").append(separator)
+                                .append("------------ Завершение работы программы... --------------").append(separator)
+                                .toString()
+                )
+        );
     }
 }
