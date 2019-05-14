288d0cd85c33479450dbbd4ccc36f0052184adf9
Ilya Balov <ilays2011@gmail.com>
2019-05-14 21:33:48 +0300

    1. Конвертация листа массивов в один лист Integer[#136032]

--- chapter_003/src/main/java/ru/job4j/list/ConvertMatrix2List.java
+++ chapter_003/src/main/java/ru/job4j/list/ConvertMatrix2List.java
@@ -14,4 +14,15 @@ public class ConvertMatrix2List {
         }
         return list;
     }
+
+    public List<Integer> convert(List<int[]> list) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int[] array : list) {
+            for (int num : array) {
+                result.add(num);
+            }
+        }
+        return result;
+    }
 }
--- chapter_003/src/test/java/ru/job4j/list/ConvertMatrix2ListTest.java
+++ chapter_003/src/test/java/ru/job4j/list/ConvertMatrix2ListTest.java
@@ -2,6 +2,7 @@ package ru.job4j.list;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,6 +19,17 @@ public class ConvertMatrix2ListTest {
         };
         List<Integer> expect = Arrays.asList(1, 2, 3, 4);
         List<Integer> result = list.toList(input);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenConvertListOfArrays() {
+        ConvertMatrix2List converter = new ConvertMatrix2List();
+        List<int[]> list = new ArrayList<>();
+        list.add(new int[]{1, 2, 3, 4});
+        list.add(new int[]{5, 6});
+        List<Integer> expect = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> result = converter.convert(list);
         assertThat(result, is(expect));
     }
 }
